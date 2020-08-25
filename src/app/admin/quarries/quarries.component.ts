98430af02822c63c3e83da9723a066ffa1055ccb
Muhammad Rehan <rehan@NANO-PC6>
2020-08-25 16:56:32 +0400

    init Commit

---
+++ src/app/admin/quarries/quarries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quarries',
+  templateUrl: './quarries.component.html',
+  styleUrls: ['./quarries.component.css']
+})
+export class QuarriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './admin/home/home.component';
+import { QuarriesComponent } from './admin/quarries/quarries.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './admin/component/nav/nav.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home', children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'quarries', component: QuarriesComponent }
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    QuarriesComponent,
+    LoginComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
